66a9cfb209c7771526dcdea4a0ab96ae1d549273
Andrei <Andrei@Home>
2020-03-17 20:16:28 -0700

    New spine model, reads file system for values

--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var fs = require('fs-extra');
 
 const PORT = 7800;
 
@@ -12,6 +13,21 @@ app.use('/spine',express.static(path.join(__dirname,'spine')));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/site/spine-demo.html'));
+});
+
+getSkinOptions = () => {
+    // assume the skins directory is in static
+    const root = './spine/default/';
+    let items = {};
+    let meshes = fs.readdirSync(root);
+    for (let i in meshes) {
+        items[meshes[i]] = fs.readdirSync(root + meshes[i]);
+    }
+    return items;
+}
+
+app.get('/skins', (req, res) => {
+    res.send(JSON.stringify(getSkinOptions()));
 });
 
 var server = http.Server(app);
